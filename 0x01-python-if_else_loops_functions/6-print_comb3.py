008406ca34333fb7fdf0a10c1fbedf51e654e317
Abdi Berhe <abdi201380@gmail.com>
2023-03-07 17:48:44 +0300

    post checker

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ for i in range(9):
             if i == 8 and j == 9:
                 print('{}{}'.format(i, j))
             else:
-                print('{}{}}'.format(i, j), end=', ')
+                print('{}{}'.format(i, j), end=', ')
         else:
             continue
