21e10161c6fc6bef10f6e1994e28ccac4e3fafe1
Abdi Berhe <abdi201380@gmail.com>
2023-03-07 23:35:21 +0300

    c file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -8,32 +9,28 @@
  *
  * Return: The address of the new node, or NULL if it failed.
  */
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *new_node = NULL, *current = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->n = number;
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	while (current->next != NULL && current->next->n < number)
-	{
-		current = current->next;
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+listint_t* insert_node(listint_t** head, int number) {
+    listint_t* new_node = (listint_t*)malloc(sizeof(listint_t));
+    if (new_node == NULL) {
+        return NULL;
+    }
+    new_node->data = number;
+    new_node->next = NULL;
+    if (*head == NULL) {
+        *head = new_node;
+        return new_node;
+    }
+    if (number < (*head)->data) {
+        new_node->next = *head;
+        *head = new_node;
+        return new_node;
+    }
+    listint_t* current = *head;
+    while (current->next != NULL && current->next->data < number) {
+        current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+    return new_node;
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,17 +2,24 @@
 #define LISTS_H
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct list_node - singly linked list
+ * @data: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
+/*
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
+} listint_t;
+*/
+
+typedef struct list_node {
+    int data;
+    struct list_node* next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
