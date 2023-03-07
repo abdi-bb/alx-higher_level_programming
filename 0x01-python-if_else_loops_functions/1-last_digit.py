cc1376de942c40a832c4a26832c2b0f8284e8dff
Abdi Berhe <abdi201380@gmail.com>
2023-03-07 16:54:00 +0300

    correcting string format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ else:
     last_digit = abs(number) % 10
 
 if last_digit > 5:
-    print(f'Last digit of {} is {} and is greater than 5'.format(number, last_digit))
+    print('Last digit of {} is {} and is greater than 5'.format(number, last_digit))
 elif last_digit == 0:
-    print(f'Last digit of {} is {} and is 0'.format(number, last_digit))
+    print('Last digit of {} is {} and is 0'.format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
-    print(f'Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
+    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, last_digit))
