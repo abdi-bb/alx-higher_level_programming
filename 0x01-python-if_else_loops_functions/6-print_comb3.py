e3adf84808041d04c1fb1fa30511769f0f26b1b9
Abdi Berhe <abdi201380@gmail.com>
2023-03-07 08:47:18 +0000

    combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(10):
+        if i < j:
+            if i == 8 and j == 9:
+                print(f'{i}{j}')
+            else:
+                print(f'{i}{j}', end=', ')
+        else:
+            continue
+
