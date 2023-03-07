8a3b0eb82ed50cbaf41a87cc8f62f64aa085f18b
Abdi Berhe <abdi201380@gmail.com>
2023-03-07 17:46:23 +0300

    after checker

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 ascii_number = 97
 while ascii_number <= 122:
     if chr(ascii_number) != 'q' and chr(ascii_number) != 'e':
-        print(chr(ascii_number), end='')
+        print('{}'.format(chr(ascii_number)), end='')
     ascii_number += 1
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for item in range(0, 100):
     if item == 99:
-        print(f'{item // 10}{item % 10}')
+        print('{}{}'.format((item // 10), (item % 10)))
     else:
-        print(f'{item // 10}{item % 10}', end=', ')
+        print('{}{}'.format((item // 10), (item % 10)), end=', ')
