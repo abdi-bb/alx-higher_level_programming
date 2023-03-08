f577ec310296ae88bc3b04755d4601e6e4b8e4dc
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 01:30:57 +0300

    formatting file

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,74 +1,33 @@
-/*
 #include <stdlib.h>
 #include "lists.h"
 
-
+/**
  * insert_node - Inserts a number into a sorted singly linked list.
  * @head: A pointer to the head of the list.
  * @number: The number to insert.
  *
  * Return: The address of the new node, or NULL if it failed.
- 
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *new_node = NULL, *current = *head;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->n = number;
-	new_node->next = NULL;
-
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	while (current->next != NULL && current->next->n < number)
-	{
-		current = current->next;
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
-}
-*/
-
-#include <stdlib.h>
-#include "lists.h"
-
-/**
- * *insert_node - inserts a number into a sorted singly linked list
- * @head: pointer to address of head of list
- * @number: integer to be include in new node
- * Return: address of new node, or NULL if it failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *newnode, *temp;
+	listint_t *new_node, *temp;
 
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
 	if (*head == NULL)
 	{
-		newnode->n = number;
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
+		new_node->n = number;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	else if (number <= (*head)->n)
 	{
-		newnode->n = number;
-		newnode->next = *head;
-		*head = newnode;
-		return (newnode);
+		new_node->n = number;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	else
 	{
@@ -77,10 +36,10 @@ listint_t *insert_node(listint_t **head, int number)
 		{
 			temp = temp->next;
 		}
-		newnode->n = number;
-		newnode->next = temp->next;
-		temp->next = newnode;
-		return (newnode);
+		new_node->n = number;
+		new_node->next = temp->next;
+		temp->next = new_node;
+		return (new_node);
 	}
 	return (NULL);
 }
