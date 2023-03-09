58d548f7fbcf7f6cdc4affb7dbc74332cf735084
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 11:38:03 -0800

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import magic_calculation_102
 
+
 def magic_calculation(a, b):
     add = magic_calculation_102.add
     sub = magic_calculation_102.sub
@@ -10,5 +11,5 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return c
-    
+
     return sub(a, b)
