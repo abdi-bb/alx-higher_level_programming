a4fd7d67997dd2176e0d19114925841f18c6f217
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 08:45:23 -0800

    updating file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         print('1 argument:')
     else:
         print('{} arguments:'.format(arg_numbers - 1))
-    for item in range(arg_numbers):
-        print('{}: {}'.format(item + 1, argv[item]))
+    for item in range(1, arg_numbers):
+        print('{}: {}'.format(item, argv[item]))
