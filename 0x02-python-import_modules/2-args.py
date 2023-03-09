95c7a3f84c683ef2bf5eca89d8bfece0b8761798
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 09:06:38 -0800

    updating file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from sys import argv
 
-    arg_numbers = len(argv) - 1 
+    arg_numbers = len(argv) - 1
     if arg_numbers == 0:
-        print('0 arguments.')
+        print("0 arguments.")
     elif arg_numbers == 1:
-        print('1 argument:')
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(arg_numbers))
+        print("{} arguments:".format(arg_numbers))
     for item in range(arg_numbers):
-        print('{}: {}'.format(item + 1, argv[item + 1]))
+        print("{}: {}".format(item + 1, argv[item + 1]))
