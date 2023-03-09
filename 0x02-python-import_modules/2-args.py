767fe8fd4510a504fc31b6d646c71f7aefa41ae3
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 08:18:02 -0800

    updating file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    arg_numbers = len(argv)
+    if arg_numbers == 0:
+        print('0 arguments.')
+    elif arg_numbers == 1:
+        print('1 argument:')
+    else:
+        print('{} arguments:'.format(arg_numbers))
+    for item in range(arg_numbers):
+        print('{}: {}'.format(item + 1, argv[item + 1]))
