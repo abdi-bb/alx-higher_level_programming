637311af2ae35e266297718c294fe2557bd01551
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 10:19:40 -0800

    calculator using command arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
 if __name__ == '__main__':
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
 
-    arg_numbers = len(argv)
-    a = argv[1]
-    b = argv[4]
-    operator = argv[3]
+    arg_numbers = len(sys.argv)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
 
     if arg_numbers != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
+        sys.exit(1)
     else:
         if operator == '+':
             print('{} + {} = {}'.format(a, b, add(a, b)))
-            exit(0)
         elif operator == '-':
             print('{} - {} = {}'.format(a, b, sub(a, b)))
-            exit(0)
         elif operator == '*':
             print('{} * {} = {}'.format(a, b, mul(a, b)))
-            exit(0)
         elif operator == '/':
             print('{} / {} = {}'.format(a, b, div(a, b)))
-            exit(0)
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
