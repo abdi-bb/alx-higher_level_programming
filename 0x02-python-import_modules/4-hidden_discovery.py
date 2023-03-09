732d6256c5471b17c37adb0fe2d8f6f7709344c0
Abdi Berhe <abdi201380@gmail.com>
2023-03-09 10:46:45 -0800

    hidden funcs

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ = '__main__':
+if __name__ == "__main__":
     import hidden_4
 
     funcs = dir(hidden_4)
-    for items in funcs:
-        if items[:2] != '__':
-            print(items)
+    for item in funcs:
+        if item[:2] != "__":
+            print(item)
