f2da4d187f9d7ca6266a18492ad075d7760d9c3f
Abdi Berhe <abdi201380@gmail.com>
2023-03-10 08:33:08 -0800

    adding readme and first task

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for item in my_list:
-        print('{}'.format(i))
+        print('{}'.format(item))
+if __name__ == '__main__':
+    print_list_integer(my_list=[])
