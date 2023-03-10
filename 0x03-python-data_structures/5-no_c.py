1170be9b057cea4ada1c9d3fd782984942e8e9a1
Abdi Berhe <abdi201380@gmail.com>
2023-03-10 10:49:32 -0800

    destroy c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for item in my_string:
+        if item != 'c' and item != 'C':
+            new_string += item
+
+    return new_string
+
+
+if __name__ == '__main__':
+    no_c(my_string)
