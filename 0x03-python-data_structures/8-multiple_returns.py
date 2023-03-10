d078a43577da31b15439d3b870ca95ff4b7ccb10
Abdi Berhe <abdi201380@gmail.com>
2023-03-10 13:29:59 -0800

    length and first char

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        sentence[0] = None
+    tuple_formed = (len(sentence), sentence[0])
+    return tuple(tuple_formed)
+
+
+if __name__ == '__main__':
+    multiple_returns(sentence)
