400cb5455519d90605ccb66f141564917299d61c
Abdi Berhe <abdi201380@gmail.com>
2023-03-11 03:03:22 -0800

    fufilling bettycodestyle

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,53 +4,69 @@
 
 
 /**
- * is_palindrome - checks if a singly linked list of integers is a palindrome
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
  *
- * @head: pointer to the head node of the linked list
- *
- * Return: 1 if the linked list is a palindrome, 0 otherwise
+ * Return: A pointer to the head of the reversed list.
  */
-int is_palindrome(listint_t *head)
+listint_t *reverse_listint(listint_t **head)
 {
-	if (head == NULL || head->next == NULL)
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
 	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
 	}
 
-	listint_t* slow = head;
-	listint_t* fast = head;
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
 
-	while (fast->next != NULL && fast->next->next != NULL)
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-
-	listint_t* prev = NULL;
-	listint_t* curr = slow->next;
-	listint_t* next;
-
-	while (curr != NULL)
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
-
-	listint_t* p1 = head;
-	listint_t* p2 = prev;
-
-	while (p2 != NULL)
-	{
-		if (p1->n != p2->n)
-		{
+		if (tmp->n != rev->n)
 			return (0);
-		}
-		p1 = p1->next;
-		p2 = p2->next;
+		tmp = tmp->next;
+		rev = rev->next;
 	}
+	reverse_listint(&mid);
 
 	return (1);
 }
-
