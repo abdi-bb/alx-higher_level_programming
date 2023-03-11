c59a1246e43e012f386dcf3c808fd098d0578518
Abdi Berhe <abdi201380@gmail.com>
2023-03-11 00:47:33 -0800

    optimizing code

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if sentence == '':
         sentence[0] = None
     tuple_formed = (len(sentence), sentence[0])
     return tuple(tuple_formed)
