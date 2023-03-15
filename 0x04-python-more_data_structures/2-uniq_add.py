abc9ac8de2a3db401c6b025751a551c7c70ebe04
Abdi Berhe <abdi201380@gmail.com>
2023-03-15 19:06:39 +0300

    add unique numbers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,11 +3,12 @@ from functools import reduce
 
 
 def uniq_add(my_list=[]):
-    new_list = []
-    for i in my_list:
-        if i not in new_list:
-            new_list.append(i)
-    result = reduce(lambda x, y: x + y, new_list)
+    if my_list:
+        new_list = []
+        for i in my_list:
+            if i not in new_list:
+                new_list.append(i)
+        result = reduce(lambda x, y: x + y, new_list)
     return result
 
 
