b0bb2b94372b3d066510b724854fd07f188fa53a
Abdi Berhe <abdi201380@gmail.com>
2023-03-15 20:30:59 +0300

    returning differences

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return ((set_1 - set_2).union(set_2 - set_1))
+    return ((set_1.union(set_2)) - (set_1.inersection(set_2)))
 
 
 if __name__ == '__main__':
