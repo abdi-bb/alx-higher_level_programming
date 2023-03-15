cdaee3809e2ed01e9b650b37f52b8d40d7779f0e
Abdi Berhe <abdi201380@gmail.com>
2023-03-15 20:35:47 +0300

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    common_set = set_1.intersection(set_2)
-    # for i in set_1:
-        # if i in set_2:
-            # common_set.append(i)
+    common_set = list(set_1.intersection(set_2))
+
     return common_set
 
 
