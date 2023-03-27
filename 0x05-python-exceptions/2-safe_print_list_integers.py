314d468bd43aa5e3232781f1bb4a5fd281c65d70
Abdi Berhe <abdi201380@gmail.com>
2023-03-27 06:31:02 -0700

    printing int from a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(i, int):
                 print('{:d}'.format(i), end='')
                 count += 1
-        except (TypeError, IndexError):
+        except (IndexError):
             pass
         if count == x:
             break
