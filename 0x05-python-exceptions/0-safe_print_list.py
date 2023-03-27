91c9ce8e8f9b9b1682490864f558eb117ea69c68
Abdi Berhe <abdi201380@gmail.com>
2023-03-27 14:09:53 +0300

    printing list elements safely using try: / except:

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while my_list[count] and count < x:
             print(my_list[count], end='')
             count += 1
-    except:
+    except IndexError:
         pass
     print()
     return count
