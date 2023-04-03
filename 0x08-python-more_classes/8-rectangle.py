e1fbc2df0feec5df8f30dd820db800b0b999cd3c
Abdi Berhe <abdi201380@gmail.com>
2023-04-03 08:33:55 -0700

    class rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -81,3 +81,14 @@ class Rectangle:
         '''prints a message when an instance of Rectangle is deleted.'''
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
