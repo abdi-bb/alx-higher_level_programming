0587bc2855a36416dcbe27775f90049fc285f3c1
Abdi Berhe <abdi201380@gmail.com>
2023-04-05 06:26:13 -0700

    after QA reviewb

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+'''
+module: '3-say_my_name'
+functions:
+    'say_my_name(first_name, last_name)'
+'''
 def say_my_name(first_name, last_name=""):
     '''Prints name
 
@@ -13,6 +19,11 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
+        print('last_name must be a string')
+
+    if (not isinstance(first_name, str) and isinstance(last_name, str)):
+        raise TypeError('first_name must be a string')
+    if (not isinstance(last_name, str) and isinstance(first_name, str)):
         print('last_name must be a string')
 
     print('My name is', first_name, last_name)
