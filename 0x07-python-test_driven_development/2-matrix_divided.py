b9765f65a497740f3dc0355b46ae9feb1c923cc1
Abdi Berhe <abdi201380@gmail.com>
2023-04-05 05:52:53 -0700

    correction from QA review

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+
+'''
+module name: '2-matrix_divided'
+functions inside the module:
+    matrix_divided(matrix, div)
+'''
+
+
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix by a given number.
@@ -8,12 +16,14 @@ def matrix_divided(matrix, div):
         div: a number (integer or float) to divide the matrix elements by.
 
     Returns:
-        A new matrix with all elements of the input matrix divided by div, rounded to 2 decimal places.
+        A new matrix with all elements of the input matrix divided by div,
+        rounded to 2 decimal places.
 
     Raises:
         TypeError: if the matrix is not a list of lists of integers or floats,
                   or if each row of the matrix is not of the same size.
-                  The error message will be "matrix must be a matrix (list of lists) of integers/floats"
+                  The error message will be
+                  "matrix must be a matrix (list of lists) of integers floats"
                   or "Each row of the matrix must have the same size".
         TypeError: if div is not a number (integer or float).
                   The error message will be "div must be a number".
@@ -24,18 +34,20 @@ def matrix_divided(matrix, div):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
     size = None
-    for l in matrix:
-        if type(l) is not list:
+    for lst in matrix:
+        if type(lst) is not list:
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
-            size = len(l)
-        elif size != len(l):
+            size = len(lst)
+        elif size != len(lst):
             raise TypeError("Each row of the matrix must have the same size")
-        for i in l:
+        for i in lst:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-                        integers/floats")
+                msg1 = 'matrix must be a matrix (list of lists)'
+                msg2 = ' of integers/floats'
+                msg = msg1 + msg2
+                raise TypeError(msg)
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
