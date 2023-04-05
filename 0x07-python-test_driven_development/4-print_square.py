2041ab7834f9492685c685f45ff7088a8bca5753
Abdi Berhe <abdi201380@gmail.com>
2023-04-05 07:12:07 -0700

    adding extra if condition to check size > 0

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -22,6 +22,7 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    for i in range(size):
-        [print('#', end='') for j in range(size)]
-        print('')
+    if size > 0:
+        for i in range(size):
+            [print('#', end='') for j in range(size)]
+            print()
