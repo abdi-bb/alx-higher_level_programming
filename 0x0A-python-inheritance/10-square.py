010ea5bf2804ae6f22fd78adc8826f7d3e5e2903
Abdi Berhe <abdi201380@gmail.com>
2023-04-10 14:13:57 -0700

    class Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,39 +8,7 @@ Contained functions or subclass:
 '''
 
 
-class BaseGeometry:
-    '''Represents BaseGeometry'''
-
-    def integer_validator(self, name, value):
-        '''Validates value'''
-        if not isinstance(value, int):
-            raise TypeError(f'{name} must be an integer')
-        if value <= 0:
-            raise ValueError(f'{name} must be greater than 0')
-
-
-class Rectangle(BaseGeometry):
-    '''Represents Rectangle'''
-
-    def __init__(self, width, height):
-        '''Instantiates new Rectangle
-
-        Args:
-            width (int): width of rectangle
-            height (int): height of rectangle
-        '''
-        self.__width = width
-        self.__height = height
-        self.integer_validator('width', width)
-        self.integer_validator('height', height)
-
-    def area(self):
-        '''returns area of Rectangle'''
-        return self.__width * self.__height
-
-    def __str__(self):
-        '''returns [Rectangle] <width>/<height>'''
-        return f'[Rectangle] {self.__width}/{self.__height}'
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
