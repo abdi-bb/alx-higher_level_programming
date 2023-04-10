f305cd47e5be0f55d5ddec92d71f46a1ca004cb1
Abdi Berhe <abdi201380@gmail.com>
2023-04-10 17:36:33 +0000

    raises Exceprion with a message

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+Module name: '6-base_geometry'
+Contained functions:
+    base_geometry
+'''
+
+
+class BaseGeometry:
+    '''Represents BaseGeometry'''
+
+    def area(self):
+        '''raises Exception with a message'''
+        raise Exception('area() is not implemented')
