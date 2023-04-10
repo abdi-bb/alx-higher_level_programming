1594213ba732c17434c305423840908729f35e8d
Abdi Berhe <abdi201380@gmail.com>
2023-04-10 13:09:57 -0700

    implementing area and dundur str

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+'''
+Module name: '8-rectangle'
+Contained functions or subclass:
+    subclass Rectangle
+'''
+
+
+class BaseGeometry:
+    '''Represents BaseGeometry'''
+
+    def integer_validator(self, name, value):
+        '''Validates value'''
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    '''Represents Rectangle'''
+
+    def __init__(self, width, height):
+        '''Instantiates new Rectangle
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        '''
+        self.__width = width
+        self.__height = height
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+
+    def area(self):
+        '''returns area of Rectangle'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''returns [Rectangle] <width>/<height>'''
+        return f'[Rectangle] {self.__width}/{self.__height}'
