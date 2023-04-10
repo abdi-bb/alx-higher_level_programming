adabaec5f5f3186477e7e2fc455604100bf25e26
Abdi Berhe <abdi201380@gmail.com>
2023-04-10 12:51:49 -0700

    inheritance starts

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+'''
+Module name: '8-rectangle'
+Contained functions or subclass:
+    subclass Rectangle
+'''
+
+
+class BaseGeometry:
+    '''Represents BaseGeometry'''
+
+    def area(self):
+        '''Raises Exception'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''Validates value'''
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    '''Represents Rectangle'''
+
+    def __init__(self, width, height):
+        '''Instantiates new Rectangle
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        '''
+        self.__width = width
+        self.__height = height
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
