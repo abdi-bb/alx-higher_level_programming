9aac006181cad3d22ccd0e55a6e0ec16ede35e28
Abdi Berhe <abdi201380@gmail.com>
2023-04-10 07:31:36 -0700

    checks if object is subclass of a specified class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+Module name: '4-inherits_from'
+Contained functions:
+    inherits_from
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    Checks subclass
+    '''
+    return issubclass(obj, a_class)
