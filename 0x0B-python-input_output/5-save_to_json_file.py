faa38ab127459b98fcb1eb90183317a6547450cb
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 16:52:04 +0000

    json on text file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,6 +13,5 @@ def save_to_json_file(my_obj, filename):
     '''
     writes to a text file using json representation
     '''
-    jfile = json.dumps(my_obj)
     with open(filename, 'w', encoding='utf-8') as f:
-        data = f.write(jfile)
+        json.dump(my_obj, f)
