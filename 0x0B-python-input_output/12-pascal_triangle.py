96075277484a092d29e34485f45289ec3e246d8d
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 19:21:41 +0000

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+module: '12-pascal_triangle'
+funcs:
+    pascal_triangle
+'''
+
+
+def pascal_triangle(n):
+    '''Returns pascal's triangle'''
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[
+                i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
