6acc2c21b55e501db7f2b4a4eb37fadbfced3814
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 14:59:05 +0000

    reads from a file and prints it

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+Module: '0-read_file'
+Functions:
+    read_file
+'''
+
+
+def read_file(filename=""):
+    '''Reads from a file and prints it to stdout.
+
+    Args:
+        filename (str): the file to be opened
+    '''
+    with open("filename", encoding='utf-8') as f:
+        data = f.read()
+        print(data)
