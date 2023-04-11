e95d2f308a830f315ad9448716fa67f8fd8360fa
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 19:57:23 +0000

    reads line by line and computes metrices

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,34 +9,35 @@ Functions:
 
 import sys
 
-status_counts = {"200": 0, "301": 0, "400": 0, "
-        401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-
-total_file_size = 0
-
-line_count = 0
+status_codes = {}
+total_size = 0
+count = 0
 
 try:
     for line in sys.stdin:
-        parts = line.split()
+        count += 1
+        split_line = line.split()
 
-        file_size = int(parts[-1])
-        status_code = parts[-2]
+        if len(split_line) == 7:
+            file_size = int(split_line[6])
+            status_code = int(split_line[5])
 
-        total_file_size += file_size
-        if status_code in status_counts:
-            status_counts[status_code] += 1
+            total_size += file_size
 
-        line_count += 1
+            if status_code not in status_codes:
+                status_codes[status_code] = 1
+            else:
+                status_codes[status_code] += 1
 
-        if line_count % 10 == 0:
-            print(f"File size: {total_file_size}")
-            for code in sorted(status_counts.keys()):
-                if status_counts[code] > 0:
-                    print(f"{code}: {status_counts[code]}")
+        if count % 10 == 0:
+            print("File size: {}".format(total_size))
+            for code in sorted(status_codes.keys()):
+                print("{}: {}".format(code, status_codes[code]))
+
+        if count % 50 == 0:
+            status_codes = {}
 
 except KeyboardInterrupt:
-    print(f"\nFile size: {total_file_size}")
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
