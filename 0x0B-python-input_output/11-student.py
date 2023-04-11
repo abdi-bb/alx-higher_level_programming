5c0d5b6e4f386214f8c29fd34c69f56f3fb61175
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 19:14:20 +0000

    class student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,15 +15,16 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-    
+        return {attr: getattr(self, attr) for
+                attr in attrs if hasattr(self, attr)}
+
     def reload_from_json(self, json):
         for key, value in json.items():
             setattr(self, key, value)
-    
+
     def __str__(self):
         return "{} {} - {}".format(self.first_name, self.last_name, self.age)
