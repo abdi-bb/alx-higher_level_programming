4812442ea5bd58a4ffb8bbdd0d5898ff8eb67edc
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 16:01:48 +0000

    to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,8 +9,16 @@ funcs:
 '''
 
 
+
 def to_json_string(my_obj):
     '''
-    Returns json representation of an object(string).
+    Returns the JSON representation of a Python object.
+
+    Args:
+        my_obj: A Python object.
+
+    Returns:
+        A JSON string representing the object.
     '''
     return json.dumps(my_obj)
+
