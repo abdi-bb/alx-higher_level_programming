4d69b0c29ff48f9cac109bcdbe06cbfe019ef4c5
Abdi Berhe <abdi201380@gmail.com>
2023-04-11 18:40:37 +0000

    reads line by line and computes metrices

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,6 +6,7 @@ Functions:
     stats
 '''
 
+
 import sys
 import signal
 
@@ -14,10 +15,21 @@ def signal_handler(sig, frame):
     print_stats()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
 
 total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+status_code_counts = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+        }
+
 
 def print_stats():
     print(f"File size: {total_file_size}")
@@ -25,8 +37,11 @@ def print_stats():
         if count > 0:
             print(f"{code}: {count}")
 
+
 for i, line in enumerate(sys.stdin):
-    ip, _, _, status_code, file_size = line.split()[0], line.split()[8], line.split()[10], line.split()[11], line.split()[12]
+    ip, _, _, status_code, file_size = line.split()[0],
+    line.split()[8], line.split()[10],
+    line.split()[11], line.split()[12]
     total_file_size += int(file_size)
     status_code_counts[int(status_code)] += 1
     if (i+1) % 10 == 0:
