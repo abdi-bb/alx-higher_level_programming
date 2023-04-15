a95babb1aebd416a59eabd915315d6229be4c1be
Abdi Berhe <abdi201380@gmail.com>
2023-04-15 08:09:32 -0700

    assigning to each attribue from multiple args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,15 +97,19 @@ class Rectangle(Base):
         b = self.__height
         return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - {a}/{b}'
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         '''Assigns an argument to each attribute'''
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.__width = args[1]
-        if len(args) > 2:
-            self.__height = args[2]
-        if len(args) > 3:
-            self.__x = args[3]
-        if len(args) > 4:
-            self.__y = args[4]
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+        else:
+            for k, v in kwargs:
+                self.id[k]
