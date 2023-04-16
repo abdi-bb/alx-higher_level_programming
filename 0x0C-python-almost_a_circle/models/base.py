c557901ebbbc861ede0dff648569bf48451a6a06
Abdi Berhe <abdi201380@gmail.com>
2023-04-16 03:30:53 -0700

    json_string to pyhton dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,9 +22,29 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''Returns the JSON string representation of list_dictionaries'''
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''Writes the JSON string representation of list_objs to a file'''
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w', encoding='utf-8') as f:
+            if list_objs is None:
+                f.write('[]')
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+                f.write(cls.to_json_string(dict_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of JSON string representation json_string'''
+        if json_string is None or len(json_string) == 0:
+            return '[]'
+        else:
+            return json.loads(json_string)
