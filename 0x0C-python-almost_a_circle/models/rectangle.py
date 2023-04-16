36f36a4b84db88930365ba502bd993560c5e6ffb
Abdi Berhe <abdi201380@gmail.com>
2023-04-16 01:42:01 -0700

    Dictionary representation of a Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -122,3 +122,8 @@ class Rectangle(Base):
                     self.__x = value
                 if key == 'y':
                     self.__y = value
+
+    def to_dictionary(self):
+        '''Returns the dictionary representation of the rectangle'''
+        return {'id': self.id, 'width': self.__width, 'height':
+                self.__height, 'x': self.__x, 'y': self.__y}
