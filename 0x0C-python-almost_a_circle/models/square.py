4473a3bb09b0c7d92e7cdd43626ee98e57e08aff
Abdi Berhe <abdi201380@gmail.com>
2023-04-17 01:54:22 -0700

    optimizing code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,8 +18,8 @@ class Square(Rectangle):
 
     def __str__(self):
         '''Returns the described string format'''
-        return '[Square] ({}) {}/{} - {}'.format(
-                self.id, self.x, self.y, self.width)
+        return '[{}] ({}) {}/{} - {}'.format(
+                type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
@@ -29,34 +29,28 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         '''setter for size attribute'''
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
         '''That assigns multiple arguments to the attributes'''
-        if args:
-            if len(args) > 0:
+        if args and len(args) != 0:
+            try:
                 self.id = args[0]
-            if len(args) > 1:
-                self.width = args[1]
-                self.height = args[1]
-            if len(args) > 2:
+                self.size = args[1]
                 self.x = args[2]
-            if len(args) > 3:
                 self.y = args[3]
-
+            except IndexError:
+                pass
         else:
-            for k, v in kwargs.items():
-                if k == 'id':
-                    self.id = v
-                if k == 'size':
-                    self.width = v
-                    self.height = v
-                if k == 'x':
-                    self.x = v
-                if k == 'y':
-                    self.y = v
+            for key, value in kwargs.items():
+                self.__setattr__(key, value)
 
     def to_dictionary(self):
         '''Dictionary representation of a Square'''
-        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
+        return {'id': getattr(self, "id"), 'size': getattr(
+            self, "width"), 'x': getattr(self, "x"), 'y': getattr(self, "y")}
