96211d1add74b119109a4d6ab765ad363f04827c
Abdi Berhe <abdi201380@gmail.com>
2023-05-16 15:43:54 -0700

    [Script]: ave temp of cities

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -3,10 +3,8 @@
 -- mysql -h localhost -u root -p hbtn_0c_0 <<EOF
 source temperatures.sql;
 
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperature
-WHERE name IS NOT NULL
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE city IS NOT NULL
 GROUP BY city
 ORDER BY avg_temp DESC;
-EOF
-
