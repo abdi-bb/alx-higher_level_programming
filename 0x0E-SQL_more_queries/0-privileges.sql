3bac6fd7bbe6b35c7dfd9f19cd044f462c6fe0b3
Abdi Berhe <abdi201380@gmail.com>
2023-05-17 08:14:30 -0700

    [Script]: lists privilages of given users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- on your server (in localhost)
+SELECT *
+FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
