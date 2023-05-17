d12cf8a2e945db2cc02bbc20008494bb338ee5ee
Abdi Berhe <abdi201380@gmail.com>
2023-05-17 14:15:35 -0700

    [Script]: lists all shows contained in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres, tv_genres
-WHERE tv_show_genres.show_id = tv_shows.id AND tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC
+FROM tv_shows, tv_show_genres
+WHERE tv_show_genres.show_id = tv_shows.id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
