f687fa4fa81fb86619bde2013d978ee2e54fc34c
Abdi Berhe <abdi201380@gmail.com>
2023-05-17 08:19:44 -0700

    [Script]: lists privilages of given users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,6 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- on your server (in localhost)
-SELECT *
+SELECT CONCAT('Grants for ', user, '@', host) AS 'User and Host',
+	Grant_priv AS 'Privileges'
 FROM mysql.user
 WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost';
