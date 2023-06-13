e55d349e8a8b9bf1c44575719be257281c81a3a7
Abdi Berhe <abdi201380@gmail.com>
2023-06-13 13:34:29 -0700

    [Update]: adding rotate and double methods to the class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return 'Rectangle {}';
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+}
+
+module.exports = Rectangle;
