1f8b08633df9e958c73c0d4e52099efc82149170
Abdi Berhe <abdi201380@gmail.com>
2023-06-13 15:30:28 -0700

    [Add]: reverses a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const idx = list.length;
   let revList = [];
-  for (let i = idx; i > 0; i--) {
+  for (let i = list.length; i > 0; i--) {
     revList.push(list[i]);
   }
   console.log(revList);
