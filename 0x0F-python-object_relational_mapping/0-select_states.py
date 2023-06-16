90f19faa620806e3d9ea6044c811e041c8255a07
Abdi Berhe <abdi201380@gmail.com>
2023-06-16 13:25:00 -0700

    [Add]: Selects all states from a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+'''
+module: '0-select_states'
+    Selects all states from a database
+'''
+
+
+import MySQLdb
+import sys
+
+mysql_user = sys.argv[1]
+mysql_passwd = sys.argv[2]
+db_name = sys.argv[3]
+
+db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=mysql_user,
+        passwd=mysql_passwd,
+        db=db_name
+        )
+
+cursor = db.cursor()
+query = "SELECT * FROM states ORDER BY id ASC"
+cursor.execute(query)
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
+db.close
