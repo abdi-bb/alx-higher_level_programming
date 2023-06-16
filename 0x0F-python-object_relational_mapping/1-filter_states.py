f6b7f6b6435c1f818794296f22e8441b7c0c844e
Abdi Berhe <abdi201380@gmail.com>
2023-06-16 14:19:37 -0700

    [Add]: States starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+module: '1-filter_states'
+    States starting with N
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[
+        2], host='localhost', db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id')
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
