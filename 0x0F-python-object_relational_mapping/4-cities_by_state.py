a9a80d160d97b04ee6933e448b508b45713016db
Abdi Berhe <abdi201380@gmail.com>
2023-06-16 16:38:33 -0700

    [Add]: Displays cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''
+module: '4-cities_by_state'
+    Script that lists all cities from a database
+'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+
+    cur = db.cursor()
+    q = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = state_id
+    ORDER BY cities.id
+    """
+    cur.execute(q)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
