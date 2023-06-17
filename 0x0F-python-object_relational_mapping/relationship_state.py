1431b9fe3b23650b045f347c41a5eda538a7e92a
Abdi Berhe <abdi201380@gmail.com>
2023-06-17 07:24:05 -0700

    [Add]: Create State and City

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
-
-'''
-module: 'relationship_state'
-    Class definition of a class State
-'''
-
+"""Defines the State class"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from relationship_city import City
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    '''Represents class State
-    Attributes:
-        __tablename__ (str): table name of the class
-        id (int): state id
-        name (str): state name
-    '''
+    """The State class inherits from Base"""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', back_populates='state')
+    cities = relationship("City", cascade="all, delete", back_populates="state")
