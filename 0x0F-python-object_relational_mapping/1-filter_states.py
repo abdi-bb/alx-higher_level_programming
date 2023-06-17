55043beac1f3f085f74060554dd601c38328d793
Abdi Berhe <abdi201380@gmail.com>
2023-06-17 00:14:10 -0700

    [Add]: Filtering from the states table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,14 +9,14 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[
-        2], host='localhost', port=3306, db=sys.argv[3])
-    cur = db.cursor()
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[
+        1], passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
 
-    rows = cur.fetchall()
-    for row in rows:
+    query_rows = cur.fetchall()
+    for row in query_rows:
         print(row)
 
     cur.close()
-    db.close()
+    conn.close()
