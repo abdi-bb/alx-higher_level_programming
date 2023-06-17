2d8a388964ced8947b84adb0d460dacd9e92bf1a
Abdi Berhe <abdi201380@gmail.com>
2023-06-17 01:58:06 -0700

    [Add]: define class State

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''
+module: 'model_state'
+    Class definition of a class State and clase Base
+'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''Represents class State
+    Attributes:
+        __table__ (str): table name of the class
+        id (int): state id
+        name (str): state name
+    '''
+    __table__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
