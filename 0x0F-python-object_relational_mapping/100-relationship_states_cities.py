3f7c4564ec30fbc1a8f647844892665901540551
Abdi Berhe <abdi201380@gmail.com>
2023-06-18 03:30:46 -0700

    [Update]: updating the existing classes to add relationship between them

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa"""
+"""
+Creates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa
+"""
 
 import sys
 from relationship_state import Base, State
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines the State class"""
+"""
+Defines the State class
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -9,9 +11,11 @@ Base = declarative_base()
 
 
 class State(Base):
-    """The State class inherits from Base"""
+    """
+    The State class inherits from Base
+    """
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    cities = relationship("City", backref="state", cascade="all, delete")
