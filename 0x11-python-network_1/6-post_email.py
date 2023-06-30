1bb9e995f5ebaa5c303bc1368f528ddb572fb509
Abdi Berhe <abdi201380@gmail.com>
2023-06-30 15:19:40 -0700

    [Script]: post an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,8 @@ import requests
 from sys import argv
 
 if __name__ == '__main__':
-    with requests.get(argv[1], argv[2]) as response:
+    data = {'email': argv[2]}
+    with requests.get(argv[1], data=dats) as response:
         body = response.text
 
     print(f'Your email is: {argv[2]}')
