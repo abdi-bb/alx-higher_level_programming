656da9e308be4140e9e60522df66b93b5efa61bb
Abdi Berhe <abdi201380@gmail.com>
2023-06-30 14:36:14 -0700

    [Script]: POST method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,10 +10,11 @@ import sys
 url = sys.argv[1]
 email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
 
-print("Your email is: {}".format(email))
+    print("Your email is: {}".format(email))
