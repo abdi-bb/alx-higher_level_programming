92788df2e43de760ca8b81286ac71a4a3b116186
Abdi Berhe <abdi201380@gmail.com>
2023-06-30 05:42:43 -0700

    [Function]: Finds the peak from a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 '''
 Module: '6-peak'
 '''
 
 
 def find_peak(list_of_integers):
-    '''Returns a peak from a list of integers'''
+    '''Returns the peak from a list'''
+
     if not list_of_integers:
-        return
-
-    left = 0
-    right = len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            right = mid
-        else:
-            left = mid + 1
-
-    return list_of_integers[left]
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] > list_of_integers[mid - 1] and\
+            list_of_integers[mid] > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if list_of_integers[mid + 1] > list_of_integers[mid]:
+        return find_peak(list_of_integers[mid + 1:])
+    return find_peak(list_of_integers[:mid])
