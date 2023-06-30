8a6a4e26bf09e65b54c9f553de97534d27099ae2
Abdi Berhe <abdi201380@gmail.com>
2023-06-30 14:28:19 -0700

    [Script]: POST method

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Module: '2-post_email'
+Sends the POST request andbdisplays the body of the response
+'''
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+
+print("Your email is: {}".format(email))
