159742b216871b97596d0fcffce945888b64e24a
Abdi Berhe <abdi201380@gmail.com>
2023-06-30 14:34:10 -0700

    [Script]: Displays the value of a variable in the header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,8 @@ Displays the value of the X-Request-Id variable found in the header response
 
 import urllib.request
 from sys import argv
+if __name__ == '__main__':
+    with urllib.request.urlopen(argv[1]) as response:
+        header = response.getheader('X-Request-Id')
 
-with urllib.request.urlopen(argv[1]) as response:
-    header = response.getheader('X-Request-Id')
-
-print(header)
+    print(header)
